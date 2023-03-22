3c3f50389aea7106b863160b12b7ddeef9b1a35e
Josh van Eyken <josh.vaneyken@gmail.com>
2023-03-22 16:10:02 -0700

    finish addNumbers

--- intro_to_callbacks.js
+++ intro_to_callbacks.js
@@ -1,49 +1,82 @@
-class Clock {
-    constructor() {
+// class Clock {
+//     constructor() {
 
-      // 1. Create a Date object.
-      let date = new Date()
+//       // 1. Create a Date object.
+//       let date = new Date()
 
-      console.log(date)
-      // 2. Store the hours, minutes, and seconds.
-      this.hr = date.getHours();
-      this.min = date.getMinutes();
-      this.sec = date.getSeconds();
+//       console.log(date)
+//       // 2. Store the hours, minutes, and seconds.
+//       this.hr = date.getHours();
+//       this.min = date.getMinutes();
+//       this.sec = date.getSeconds();
        
-      // 3. Call printTime.
-       let formatted =  this.printTime(this.hr,this.min,this.sec);
-      // 4. Schedule the tick at 1 second intervals.
-    //   setInterval((arg) => {_tick(arg)},1000)
-        setInterval(this._tick.bind(this),1000)
-    }
+//       // 3. Call printTime.
+//        let formatted =  this.printTime(this.hr,this.min,this.sec);
+//       // 4. Schedule the tick at 1 second intervals.
+//     //   setInterval((arg) => {_tick(arg)},1000)
+//         setInterval(this._tick.bind(this),1000)
+//     }
   
-    printTime(hr,min,sec) {
-      // Format the time in HH:MM:SS
-         let formatTime = `${hr}:${min}:${sec}`
-      // Use console.log to print it.
-      console.log(formatTime)
-    }
+//     printTime(hr,min,sec) {
+//       // Format the time in HH:MM:SS
+//          let formatTime = `${hr}:${min}:${sec}`
+//       // Use console.log to print it.
+//       console.log(formatTime)
+//     }
   
-    _tick() {
-      // 1. Increment the time by one second.
-        this.sec++
-        if(this.sec === 60){
-            this.sec = 0
-            this.min+=1
-        }
+//     _tick() {
+//       // 1. Increment the time by one second.
+//         this.sec++
+//         if(this.sec === 60){
+//             this.sec = 0
+//             this.min+=1
+//         }
 
-        if(this.min === 60){
-            this.min = 0
-            this.hr+= 1
-        }
-        if(this.hr === 25){
-            this.hr = 1
-            this.min = 0
-            this.sec = 0
-        }
-      // 2. Call printTime.
-      this.printTime(this.hr,this.min,this.sec)
-    }
-  }
+//         if(this.min === 60){
+//             this.min = 0
+//             this.hr+= 1
+//         }
+//         if(this.hr === 25){
+//             this.hr = 1
+//             this.min = 0
+//             this.sec = 0
+//         }
+//       // 2. Call printTime.
+//       this.printTime(this.hr,this.min,this.sec)
+//     }
+//   }
   
-  const clock = new Clock();
+//   const clock = new Clock();
+
+
+
+const readline = require("readline");
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+
+function addNumbers(sum, numsLeft, completionCallBack) {
+  if (numsLeft > 0) {
+    reader.question('Give us a number', response => {
+      // convert response to int using parseInt, save to variable
+      let responseNum = parseInt(response);
+      // update sum with response 
+      sum += responseNum
+      //  make recursive call to addNumbers, decrement sum
+      addNumbers(sum,numsLeft - 1, completionCallBack)
+      // pass in completion callback
+      // else statement, if numsLeft not greater than 0
+
+  })
+} else if (numsLeft === 0) {
+  completionCallBack(sum)
+}
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// reader.question('Give us a number', response => {
+
+// })
